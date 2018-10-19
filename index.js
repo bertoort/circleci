88fc4dfc4affaeee9a49b78b224dbb33b5c7fc84
Roberto Ortega <berto.ort@gmail.com>
2018-10-18 22:51:10 -0600

    create express app for heroku deployment

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static('app'))
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
